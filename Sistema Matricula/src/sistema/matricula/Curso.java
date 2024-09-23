0e688a4ff70b895d1b6cc358938c036e543d7f1b
Brenda Leal <100167546+endyyxs@users.noreply.github.com>
2024-09-23 15:13:59 -0300

    Atualização

--- Sistema Matricula/src/sistema/matricula/Curso.java
+++ Sistema Matricula/src/sistema/matricula/Curso.java
@@ -2,15 +2,39 @@ package sistema.matricula;
 
 public class Curso {
     private String nome;
+    private int cargahoraria;
+	private double preco;
     private int vagas;
 
-    public Curso(String nome, int vagas) {
+    public Curso(String nome, int vagas, int cargahoraria, double preco) {
         this.nome = nome;
         this.vagas = vagas;
+        this.cargahoraria = cargahoraria;
+        this.preco = preco;
     }
 
     public String getNome() {
         return nome;
+    }
+    
+    public void setNome(String nome) {
+    	this.nome = nome;
+    }
+
+    public int getCargaHoraria() {
+    	return cargahoraria;
+    }
+
+    public void setCargaHoraria(int cargahoraria) {
+    	this.cargahoraria = cargahoraria;
+    }
+
+    public double getPreco() {
+    	return preco;
+    }
+
+    public void setPreco(double preco) {
+    	this.preco = preco;
     }
 
     public boolean temVagas() {
@@ -21,5 +45,10 @@ public class Curso {
         if (temVagas()) {
             vagas--;
         }
+    }
+    
+    @Override
+    public String toString() {
+        return nome + " - Carga Horária: " + cargahoraria + "h - Preço: R$ " + preco;
     }
 }
