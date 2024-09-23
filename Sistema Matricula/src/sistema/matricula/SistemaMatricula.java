eed1a25d626765df55b3717ec71060468ff3b97c
EduardoL15 <135390481+EduardoL15@users.noreply.github.com>
2024-09-23 20:15:43 -0300

    Update SistemaMatricula.java

--- Sistema Matricula/src/sistema/matricula/SistemaMatricula.java
+++ Sistema Matricula/src/sistema/matricula/SistemaMatricula.java
@@ -17,18 +17,18 @@ public class SistemaMatricula {
     public ArrayList<Curso> getCursos() {
         return cursos;
     }
-
+    		//avisos caso tenha algum problema na matricula
     public boolean matricularAluno(Aluno aluno, Curso curso) {
         if (!aluno.validarIdade()) {
-            JOptionPane.showMessageDialog(null, "Aluno deve ter mais de 16 anos.", "Erro", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Aluno deve ter mais de 16 anos para se matricular.", "Erro", JOptionPane.WARNING_MESSAGE);
             return false;
         }
         if (!aluno.validarMatricula()) {
-            JOptionPane.showMessageDialog(null, "A matrícula deve ter no mínimo 10 caracteres.");
+            JOptionPane.showMessageDialog(null, "O número de matrícula deve conter pelo menos 10 caracteres.", "Erro", JOptionPane.WARNING_MESSAGE);
             return false;
         }
         if (!curso.temVagas()) {
-            JOptionPane.showMessageDialog(null, "Não há vagas disponíveis para este curso.", "Erro", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Não há vagas disponíveis no curso escolhido.", "Erro", JOptionPane.WARNING_MESSAGE);
             return false;
         }
         alunos.add(aluno);
