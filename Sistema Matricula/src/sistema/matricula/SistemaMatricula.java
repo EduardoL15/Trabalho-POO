2dfe5e786db973c4ae7f6d6f158b74b33890be66
Brenda Leal <100167546+endyyxs@users.noreply.github.com>
2024-09-23 15:16:12 -0300

    Atualização 2

--- Sistema Matricula/src/sistema/matricula/SistemaMatricula.java
+++ Sistema Matricula/src/sistema/matricula/SistemaMatricula.java
@@ -1,33 +1,38 @@
 package sistema.matricula;
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 public class SistemaMatricula {
     private ArrayList<Curso> cursos = new ArrayList<>();
     private ArrayList<Aluno> alunos = new ArrayList<>();
-
+    
+ // Adicionando cursos com vagas, carga horária e preço
     public SistemaMatricula() {
-        cursos.add(new Curso("Engenharia de Software", 30));
-        cursos.add(new Curso("Ciências da Computação", 30));
-        cursos.add(new Curso("Análise e Desenvolvimento de Sistemas", 30));
-        cursos.add(new Curso("Programação", 30));
+        cursos.add(new Curso("Engenharia de Software", 30, 890, 1745.74));
+        cursos.add(new Curso("Ciências da Computação", 30, 890, 1549.79));
+        cursos.add(new Curso("Análise e Desenvolvimento de Sistemas", 30, 250, 1051.01));
     }
 
     public ArrayList<Curso> getCursos() {
         return cursos;
     }
 
-    public void matricularAluno(Aluno aluno, Curso curso) throws Exception {
+    public boolean matricularAluno(Aluno aluno, Curso curso) {
         if (!aluno.validarIdade()) {
-            throw new Exception("Aluno deve ter mais de 16 anos.");
+            JOptionPane.showMessageDialog(null, "Aluno deve ter mais de 16 anos.", "Erro", JOptionPane.WARNING_MESSAGE);
+            return false;
         }
         if (!aluno.validarMatricula()) {
-            throw new Exception("A matrícula deve ter no mínimo 10 caracteres.");
+            JOptionPane.showMessageDialog(null, "A matrícula deve ter no mínimo 10 caracteres.");
+            return false;
         }
         if (!curso.temVagas()) {
-            throw new Exception("Não há vagas disponíveis para este curso.");
+            JOptionPane.showMessageDialog(null, "Não há vagas disponíveis para este curso.", "Erro", JOptionPane.WARNING_MESSAGE);
+            return false;
         }
         alunos.add(aluno);
         curso.matricularAluno();
+        return true;
     }
 }
