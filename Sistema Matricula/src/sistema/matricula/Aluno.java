61aa09397a2b111b92cf7df59e60016b45bd4328
Brenda Leal <100167546+endyyxs@users.noreply.github.com>
2024-09-23 15:12:50 -0300

    Atualização 2

--- Sistema Matricula/src/sistema/matricula/Aluno.java
+++ Sistema Matricula/src/sistema/matricula/Aluno.java
@@ -10,6 +10,30 @@ public class Aluno {
         this.matricula = matricula;
         this.idade = idade;
     }
+    
+    public String getNome() {
+    	return nome;
+    }
+
+    public void setNome(String nome) {
+    	this.nome = nome;
+    }
+
+    public String getMatricula() {
+    	return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+    	this.matricula = matricula;
+    }
+
+    public int getIdade() {
+    	return idade;
+    }
+
+    public void setIdade(int idade) {
+    	this.idade = idade;
+    }
 
     public boolean validarIdade() {
         return idade >= 16;
